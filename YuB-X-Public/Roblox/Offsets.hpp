daea610b62578aaee4bff9acc7894b79eba02971
Jaxploit <liasyour961@gmail.com>
2025-08-14 02:16:10 +0200

    Update

--- YuB-X-Public/Roblox/Offsets.hpp
+++ YuB-X-Public/Roblox/Offsets.hpp
@@ -1,20 +1,19 @@
 #pragma once
-
 #define REBASE(Address) (Address + reinterpret_cast<uintptr_t>(GetModuleHandleA(nullptr)))
 
 namespace Offsets
 {
-	const uintptr_t Print = REBASE(0x1490C90);
-	const uintptr_t TaskDefer = REBASE(0xFE5EF0);
-	const uintptr_t RawScheduler = REBASE(0x6F55720);
-	const uintptr_t OpcodeLookupTable = REBASE(0x5291F70);
+	const uintptr_t Print = REBASE(0x1493A80);
+	const uintptr_t TaskDefer = REBASE(0xFE7E60);
+	const uintptr_t RawScheduler = REBASE(0x6F42320);
+	const uintptr_t OpcodeLookupTable = REBASE(0x527BBB0);
 
 	namespace Luau
 	{
-		const uintptr_t LuaD_throw = REBASE(0x26A4240);
-		const uintptr_t Luau_Execute = REBASE(0x26D6DE0);
-		const uintptr_t LuaO_NilObject = REBASE(0x4C773B8);
-		const uintptr_t LuaH_DummyNode = REBASE(0x4C76DD8);
+		const uintptr_t LuaD_throw = REBASE(0x267D700);
+		const uintptr_t Luau_Execute = REBASE(0x26B02E0);
+		const uintptr_t LuaO_NilObject = REBASE(0x4C5F858);
+		const uintptr_t LuaH_DummyNode = REBASE(0x4C5F278);
 
 		const uintptr_t Convention = 0x3C0;
 	}
@@ -26,7 +25,7 @@ namespace Offsets
 		const uintptr_t ScriptContext = 0x3D0;
 		const uintptr_t FakeDataModelToDataModel = 0x1C0;
 
-		const uintptr_t FakeDataModelPointer = REBASE(0x6E854F8);
+		const uintptr_t FakeDataModelPointer = REBASE(0x6E72138);
 	}
 
 	namespace ExtraSpace
@@ -35,3 +34,5 @@ namespace Offsets
 		const uintptr_t Capabilities = 0x48;
 	}
 }
+
+// Dont forget to update TaskScheduler::DecryptLuaState
